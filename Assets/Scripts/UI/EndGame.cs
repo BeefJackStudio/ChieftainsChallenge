7278560014d07d311c8feb79d42dbcc76e95e54d
Danny de Bruijne <dannydb44@gmail.com>
2018-04-23 14:30:01 +0200

    End game screen

---
+++ Assets/Scripts/UI/EndGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	public List<GameObject> stars;
+	public Sprite emptyImage;
+	public Sprite acquiredImage;
+	float animationDelay = 0.5f;
+	[ReadOnly] public bool isAnimating = false;
+
+
+	[ContextMenu("End Game Test")]
+	public void EndGameTest() {
+		OnEndGame(3);
+	}
+
+	public void OnEndGame(int starAmount) {
+		if(stars.Count < 3) {
+			Debug.LogError("Could not find enough star sprites.");
+			return;
+		}
+
+		StartCoroutine(EndGameAnimation(starAmount));
+	}
+
+	public IEnumerator EndGameAnimation(int starAmount){
+		isAnimating = true;
+		for(int i = 0; i < starAmount; i++) {
+			Animator an = stars[i].GetComponent<Animator>();
+			if(an == null){ continue; }
+
+			an.Play("AquiredAnimation");
+			yield return new WaitForSeconds(animationDelay);
+		}
+		isAnimating = false;
+	}
+}
