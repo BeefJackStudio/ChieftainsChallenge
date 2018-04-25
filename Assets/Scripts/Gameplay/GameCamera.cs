2172f475edb18936679d947f451b992088b9663a
Danny de Bruijne <dannydb44@gmail.com>
2018-04-25 11:21:39 +0200

    Implement panning and zooming.

--- Assets/Scripts/Gameplay/GameCamera.cs
+++ Assets/Scripts/Gameplay/GameCamera.cs
@@ -13,15 +13,23 @@ public class GameCamera : MonoBehaviour {
     [Header("Config")]
     public Vector3 defaultOffset = new Vector3(5, 5, -25);
     public float followSpeed = 0.02f;
-    public float panSpeed = 20f;
+    public float panSpeed = 30f;
     public float zoomSpeedTouch = 0.1f;
-    public float zoomSpeedMouse = 0.5f;
+    public float zoomSpeedMouse = 15f;
     
     [Header("Restrictions")]
     public Transform leftBound;
     public Transform rightBound;
+    public Transform topBound;
+    public Transform botBound;
     public float maxZ = -9f;
-    public float minZ = -50f;
+    public float minZ = -45f;
+
+    //Panning
+    private Vector3 lastPanPosition;
+    private int panFingerId; // Touch mode only
+    private bool wasZoomingLastFrame; // Touch mode only
+    private Vector2[] lastZoomPositions; // Touch mode only
 
     private void OnValidate() {
         if(maxZ > 0) { maxZ = 0; }
@@ -66,13 +74,85 @@ public class GameCamera : MonoBehaviour {
             if(Vector3.Distance(transform.position, desiredPosition) <= 2 && levelInstance.levelState == LevelState.intro) {
                 levelInstance.levelState = LevelState.inGame;
             }
+        } else {
+            if(Input.touchSupported && Application.platform != RuntimePlatform.WebGLPlayer) {
+                HandleTouch();
+            } else {
+                HandleMouse();
+            }
         }
 
         //Apply position!
         desiredPosition.x = Mathf.Clamp(desiredPosition.x, leftBound.transform.position.x, rightBound.transform.position.x);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, botBound.transform.position.y, topBound.transform.position.y);
         desiredPosition.z = Mathf.Clamp(desiredPosition.z, minZ, maxZ);
 
         m_PreviousBallPosition = ball.transform.position;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed);
     }
+
+#region Panning and Zooming
+    private void HandleMouse() {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if(scroll != 0) {
+            ZoomCamera(scroll, zoomSpeedMouse);
+        }
+
+        if(Input.GetMouseButtonDown(0)) {
+            lastPanPosition = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(0) && !ball.dragging) {
+            PanCamera(Input.mousePosition);
+        }
+    }
+
+    private void HandleTouch() {
+        switch(Input.touchCount) {
+            case 1: 
+                //panning
+                wasZoomingLastFrame = false;
+                Touch touch = Input.GetTouch(0);
+                if(touch.phase == TouchPhase.Began) {
+                    lastPanPosition = touch.position;
+                    panFingerId = touch.fingerId;
+                } 
+                if(touch.fingerId == panFingerId && touch.phase == TouchPhase.Moved && !ball.dragging) {
+                    PanCamera(touch.position);
+                }
+                break;
+            case 2: 
+                //zooming
+                Vector2[] newPositions = new Vector2[] { Input.GetTouch(0).position, Input.GetTouch(1).position };
+                if(!wasZoomingLastFrame) {
+                    lastZoomPositions = newPositions;
+                    wasZoomingLastFrame = true;
+                } else {
+                    float newDist = Vector2.Distance(newPositions[0], newPositions[1]);
+                    float oldDist = Vector2.Distance(lastZoomPositions[0], lastZoomPositions[1]);
+                    float offset = newDist - oldDist;
+
+                    ZoomCamera(offset, zoomSpeedTouch);
+                    lastZoomPositions = newPositions;
+                }
+
+                break;
+            default:
+                wasZoomingLastFrame = false;
+                break;
+        }
+    }
+
+    private void ZoomCamera(float offset, float speed) {
+        desiredPosition.z += offset * speed;
+    }
+
+    private void PanCamera(Vector3 newPanPosition) {
+        Vector3 offset = GetComponent<Camera>().ScreenToViewportPoint(lastPanPosition - newPanPosition);
+        
+        desiredPosition.x += offset.x * panSpeed;
+        desiredPosition.y += offset.y * panSpeed;
+
+        lastPanPosition = newPanPosition;
+    }
+#endregion
 }
