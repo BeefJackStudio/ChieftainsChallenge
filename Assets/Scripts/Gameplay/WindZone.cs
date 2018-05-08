dc8b2b654712735c2c4efdf80e7e1bd4c122e4ce
Randy Schouten <randyschouten@outlook.com>
2018-05-08 14:11:52 +0200

    Added windzone prefab

---
+++ Assets/Scripts/Gameplay/WindZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class WindZone : MonoBehaviour {
+
+    public float windForce = 10;
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<GameBall>() == null) return;
+
+        collision.GetComponent<Rigidbody2D>().AddForce(transform.up * windForce);
+    }
+
+}
