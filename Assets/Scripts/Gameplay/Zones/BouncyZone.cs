e93efe3816d1236f11b5c571df97964f47333b6c
Danny de Bruijne <dannydb44@gmail.com>
2018-05-08 16:44:38 +0200

    Bouncy zone

---
+++ Assets/Scripts/Gameplay/Zones/BouncyZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BouncyZone : MonoBehaviour {
+
+	[ReadOnly] public PhysicsMaterial2D originalPhysMat;
+	public PhysicsMaterial2D zoneMaterial;
+
+	private void Awake() {
+		this.GetComponent<BoxCollider2D>().isTrigger = true;
+	}
+
+	private void OnValidate() {
+		this.GetComponent<BoxCollider2D>().hideFlags = HideFlags.HideInInspector;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.GetComponent<GameBall>() == null) return;
+		originalPhysMat = other.gameObject.GetComponent<Rigidbody2D>().sharedMaterial;
+		other.gameObject.GetComponent<Rigidbody2D>().sharedMaterial = zoneMaterial;
+		other.gameObject.GetComponent<CircleCollider2D>().sharedMaterial = zoneMaterial;
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.GetComponent<GameBall>() == null) return;
+		other.gameObject.GetComponent<Rigidbody2D>().sharedMaterial = originalPhysMat;
+		other.gameObject.GetComponent<CircleCollider2D>().sharedMaterial = originalPhysMat;
+	}
+
+}
