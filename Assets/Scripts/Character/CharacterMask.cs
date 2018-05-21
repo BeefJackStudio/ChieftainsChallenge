59aff6b20922eca68328559a1d38128fb6f580a0
Randy Schouten <randyschouten@outlook.com>
2018-05-21 17:41:27 +0200

    Fixed skull mask feathers flipping

--- Assets/Scripts/Character/CharacterMask.cs
+++ Assets/Scripts/Character/CharacterMask.cs
@@ -16,6 +16,7 @@ public class CharacterMask : MonoBehaviour {
     public Transform[] jigglebones;
     public float jiggleAmount = 1;
     public float damping = 0.5f;
+    public bool[] flipRotations;
 
     private Vector3 m_LastPosition;
     private Dictionary<Transform, Vector3> m_StartScales = new Dictionary<Transform, Vector3>();
@@ -49,10 +50,13 @@ public class CharacterMask : MonoBehaviour {
         Vector3 targetScale = m_Velocity * jiggleAmount;
         float targetRotation = m_Velocity.x * 150 * jiggleAmount;
 
-        foreach (Transform t in jigglebones) {
+        for (int i = 0; i < jigglebones.Length; i++) {
+            Transform t = jigglebones[i];
+            bool flippedJigglebone = true;
+            if (flipRotations.Length == jigglebones.Length) flippedJigglebone = flipRotations[i];
             //float rotation = Mathf.Lerp(t.rotation.eulerAngles.z, m_StartRotations[t] + targetRotation, 0.05f);
             t.localScale = Vector3.Lerp(t.localScale, m_StartScales[t] + targetScale, 0.1f);
-            t.localRotation = Quaternion.Euler(0, 180, Mathf.Lerp(t.localEulerAngles.z, m_StartRotations[t] + targetRotation, 0.05f));           
+            t.localRotation = Quaternion.Euler(0, flippedJigglebone ? 180 : 0, Mathf.Lerp(t.localEulerAngles.z, m_StartRotations[t] + targetRotation, 0.05f));           
         }
         m_LastPosition = pos;
     }
